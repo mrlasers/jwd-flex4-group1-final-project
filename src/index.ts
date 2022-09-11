8597dd80ff7558e1ef5f1b374bd371d8621b392a
Timothy Pew <mrlasers@mrlasers.com>
2022-09-11 16:14:03 -0700

    add webpack

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log("hello, world!!!!")
+
+const app = document.createElement("div")
+app.setAttribute("id", "app")
+app.innerHTML = `<h1>Hello, World!</h1>`
+
+document.body.append(app)
---
+++ webpack.config.ts
@@ -0,0 +1,45 @@
+import 'webpack-dev-server'
+
+import * as HtmlWebpackPlugin from 'html-webpack-plugin'
+import * as Path from 'path'
+import { Configuration } from 'webpack'
+import { merge } from 'webpack-merge'
+
+const commonConfig: Configuration = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: Path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [new HtmlWebpackPlugin()],
+}
+
+const devConfig: Configuration = {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    // static: {
+    //   directory: Path.join(__dirname, "public"),
+    // },
+    compress: false,
+    port: 9001,
+  },
+  performance: {
+    hints: false,
+  },
+}
+
+export default merge(commonConfig, devConfig)
