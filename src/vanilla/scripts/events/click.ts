28637e78cee9ac32fcaf7bc75c3f7d267ff4da33
Timothy Pew <mrlasers@mrlasers.com>
2022-09-21 10:24:40 -0700

    The. End. 2: Electronic Boogaloohoo

--- src/vanilla/scripts/events/click.ts
+++ src/vanilla/scripts/events/click.ts
@@ -8,19 +8,41 @@ export function clickListeners({
 }: EventProps): void {
   document.addEventListener("click", (event) => {
     const { target } = event
+    console.log("click!", target)
 
     // console.log(">>", target)
 
     if (!(target instanceof HTMLElement)) return
 
-    if (target.matches("#duedate button")) {
-      event.preventDefault()
-      event.stopPropagation()
+    // if (target.matches("#duedate button")) {
+    //   console.log("#duedate button")
+    //   event.preventDefault()
+    //   event.stopPropagation()
+
+    //   const dateEl = document.getElementById("duedate") as HTMLDataElement
+    //   dateEl.value = ""
+
+    //   return
+    // }
+
+    if (target.matches(".deleteTodo")) {
+      const todoIdElement = target.closest("[data-todo-id]")
+      if (todoIdElement instanceof HTMLElement) {
+        const todoId = todoIdElement.getAttribute("data-todo-id")
+
+        if (todoId) {
+          return dispatch?.({
+            type: "REMOVE_TODO",
+            payload: todoId,
+          })
+        }
+      }
 
       return
     }
 
     if (target.matches(".tilelist header")) {
+      console.log("tilelist header")
       event.preventDefault()
 
       // console.log("toggling active class")
@@ -29,6 +51,7 @@ export function clickListeners({
 
     // console.log("after:", target)
     if (target.matches("[data-settings]")) {
+      console.log("[data-settings]")
       const op = target.getAttribute("data-settings")
       // console.log("what is our op?", op)
 
@@ -76,22 +99,6 @@ export function clickListeners({
     if (target.matches(".tilelist *")) {
       // console.log("tilelist child", target)
       return target.closest(".tilelist")?.classList.toggle("active")
-    }
-
-    if (target.matches(".deleteTodo")) {
-      const todoIdElement = target.closest("[data-todo-id]")
-      if (todoIdElement instanceof HTMLElement) {
-        const todoId = todoIdElement.getAttribute("data-todo-id")
-
-        if (todoId) {
-          return dispatch?.({
-            type: "REMOVE_TODO",
-            payload: todoId,
-          })
-        }
-      }
-
-      return
     }
 
     if (target.matches("#duedate button")) {
